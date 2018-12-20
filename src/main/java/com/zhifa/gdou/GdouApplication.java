c6e7636a8cbca195b3e733e823da4e555c5c163f
xiaozhi止水 <877657682@qq.com>
2018-12-20 14:51:55 +0800

    首次提交

---
+++ src/main/java/com/zhifa/gdou/GdouApplication.java
@@ -0,0 +1,17 @@
+package com.zhifa.gdou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdouApplication {
+
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(GdouApplication.class, args);
+        System.out.println("======================完成========================");
+    }
+
+}
+
