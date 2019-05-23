60f852ad9deba439a996be2244f1b86d662c8bb2
梁智发 <877567682@qq.com>
2019-05-23 10:03:07 +0800

    完成后台拦截功能

--- src/main/java/com/zhifa/gdou/config/LoginConfig.java
+++ src/main/java/com/zhifa/gdou/config/LoginConfig.java
@@ -1,6 +1,9 @@
 package com.zhifa.gdou.config;
 
-import com.zhifa.gdou.model.StudentInfo;
+import com.zhifa.gdou.model.Manager;
+import com.zhifa.gdou.model.Teacher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -9,31 +12,64 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class LoginConfig implements HandlerInterceptor {
+
+    private static Logger log = LoggerFactory.getLogger(LoginConfig.class);
+    private static List<String> managerUrl = new ArrayList<>();
+    private static List<String> teacherUrl = new ArrayList<>();
+
+
     @Override
+
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
         String requestURI = httpServletRequest.getRequestURI();
-        //System.out.println(httpServletRequest.getRequestURI()+"============》preHandle");
-      /* HttpSession httpSession = httpServletRequest.getSession();
-        StudentInfo student = (StudentInfo) httpSession.getAttribute("student");
-        if(ObjectUtils.isEmpty(student)){
-            httpServletResponse.sendRedirect("/wx/main");
-            return false;
+        HttpSession session = httpServletRequest.getSession();
+        Manager manager = (Manager) session.getAttribute("manager");
+        Teacher teacher = (Teacher) session.getAttribute("teacher");
+        if (ObjectUtils.isEmpty(manager)) {
+            if (managerUrl.contains(requestURI)) {
+                log.info("过滤 管理员url=>requestURI={}", requestURI);
+                return false;
+            }
+        }
+        if (ObjectUtils.isEmpty(teacher)) {
+            if (teacherUrl.contains(requestURI)) {
+                log.info("过滤 教师url=>requestURI={}", requestURI);
+                return false;
+            }
+        }
 
-        }*/
+
+        log.info("requestURI={}", requestURI);
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
 
-       // System.out.println("============postHandle");
+        // System.out.println("============postHandle");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
-       // System.out.println("============afterCompletion");
+        // System.out.println("============afterCompletion");
+    }
+
+    static {
+        managerUrl.add("/teacher/findAllTeachers");
+        managerUrl.add("/classInfo/findAllclassInfos");
+        managerUrl.add("/student/findAllStudents");
+        managerUrl.add("/wxMoreInfo/findAllWxMoreInfos");
+        managerUrl.add("/teacher/deleteById");
+        managerUrl.add("/teacher/insertTeacherInfo");
+        managerUrl.add("/teacher/modify");
+        //=============================
+        teacherUrl.add("/student/getScore");
+        teacherUrl.add("/student/insertscore");
+
     }
 }
--- src/main/java/com/zhifa/gdou/controller/TeacherController.java
+++ src/main/java/com/zhifa/gdou/controller/TeacherController.java
@@ -25,7 +25,7 @@ import java.util.Map;
 /**
  * @Author: zhifa
  * @Date: 2019/5/20
- * @Description:  管理员对的教师后台管理
+ * @Description: 管理员对的教师后台管理
  */
 @RestController
 @RequestMapping("/teacher")
@@ -43,106 +43,108 @@ public class TeacherController {
     private ClassInfoMapper classInfoMapper;
 
     @RequestMapping("/login")
-    public Object login(String teacherNum, String teacherPassword, HttpSession httpSession){
-        Map<String,Object> map=new HashMap<>();
-        if (ObjectUtils.isEmpty(teacherNum)||ObjectUtils.isEmpty(teacherPassword)){
+    public Object login(String teacherNum, String teacherPassword, HttpSession httpSession) {
+        Map<String, Object> map = new HashMap<>();
+        if (ObjectUtils.isEmpty(teacherNum) || ObjectUtils.isEmpty(teacherPassword)) {
             map.put("code", 1);
-            map.put("msg","账号、密码不能空");
+            map.put("msg", "账号、密码不能空");
             return map;
         }
         teacherPassword = DigestUtils.md5DigestAsHex(teacherPassword.getBytes());
-        loger.info("登录密码：{}",teacherPassword);
+        loger.info("登录密码：{}", teacherPassword);
         Teacher teacher = teacherMapper.selectByNameAndPass(teacherNum, teacherPassword);
-        if (teacher==null){
+        if (teacher == null) {
             map.put("code", 1);
-            map.put("msg","账号、密码不正确");
+            map.put("msg", "账号、密码不正确");
             return map;
         }
         map.put("code", 0);
-        map.put("url","/main.html");
-        httpSession.setAttribute("teacher",teacher);
+        map.put("url", "/main.html");
+        httpSession.setAttribute("teacher", teacher);
         return map;
 
     }
 
     @RequestMapping(value = "/findAllTeachers")
-    public LayUIDataGrid findAllTeachers(@RequestParam(value = "page",defaultValue = "1") Integer page,
-                                         @RequestParam(value = "limit",defaultValue = "80")Integer limit,
-                                         @RequestParam(value = "teacherName",defaultValue = "",required = false) String teacherName){
-        PageHelper.startPage(page,limit);//分页插件
-        teacherName="%"+teacherName+"%";
+    public LayUIDataGrid findAllTeachers(@RequestParam(value = "page", defaultValue = "1") Integer page,
+                                         @RequestParam(value = "limit", defaultValue = "80") Integer limit,
+                                         @RequestParam(value = "teacherName", defaultValue = "", required = false) String teacherName) {
+        PageHelper.startPage(page, limit);//分页插件
+        teacherName = "%" + teacherName + "%";
         List<Teacher> teachers = teacherService.findAllTeachers(teacherName);
-        PageInfo<Teacher>info=new PageInfo<>(teachers);
+        PageInfo<Teacher> info = new PageInfo<>(teachers);
         long total = info.getTotal();
         List<Teacher> list = info.getList();
-       for(Teacher teacher:list){
+        for (Teacher teacher : list) {
             String teacherNum = teacher.getTeacherNum();
             String name = classInfoMapper.findClassNameByheadMasterNum(teacherNum);
-            if (name!=null){
+            if (name != null) {
                 teacher.setClassName(name);
             }
         }
-        return LayUIDataGrid.ReturnDataGrid(total,list);
+        return LayUIDataGrid.ReturnDataGrid(total, list);
     }
+
     @RequestMapping("/deleteById")
-    public Object deleteById(Integer id){
-        int n=teacherService.deleteById(id);
-        Map<String,Object> map=new HashMap<>();
-        if (n>0){
-            map.put("status",true);
+    public Object deleteById(Integer id) {
+        int n = teacherService.deleteById(id);
+        Map<String, Object> map = new HashMap<>();
+        if (n > 0) {
+            map.put("status", true);
             return map;
         }
-        map.put("status",false);
-        map.put("msg","删除失败，请重试！");
+        map.put("status", false);
+        map.put("msg", "删除失败，请重试！");
         return map;
     }
+
     @RequestMapping("/insertTeacherInfo")
-    public Object insertTeacherInfo(String teacherName,String teacherPassword){
-        Map<String,Object> map=new HashMap<>();
-        Teacher teacher=new Teacher();
+    public Object insertTeacherInfo(String teacherName, String teacherPassword) {
+        Map<String, Object> map = new HashMap<>();
+        Teacher teacher = new Teacher();
         teacher.setTeacherName(teacherName);
-        String teacherNumAndPass="gdou"+teacherName.hashCode();
+        String teacherNumAndPass = "gdou" + teacherName.hashCode();
         teacher.setTeacherNum(teacherNumAndPass);
-        if (ObjectUtils.isEmpty(teacherPassword)){
-            teacherPassword=teacherNumAndPass;
+        if (ObjectUtils.isEmpty(teacherPassword)) {
+            teacherPassword = teacherNumAndPass;
         }
         teacher.setTeacherPassword(DigestUtils.md5DigestAsHex(teacherPassword.getBytes()));
         teacher.setState(Byte.valueOf("1"));
         int insert = teacherMapper.insert(teacher);
-        if (insert>0){
+        if (insert > 0) {
             map.put("code", 0);
-            map.put("msg","添加成功！");
+            map.put("msg", "添加成功！");
             return map;
         }
         map.put("code", 1);
-        map.put("msg","失败！请重试。");
+        map.put("msg", "失败！请重试。");
 
         return map;
     }
+
     @RequestMapping("/modify")
     @Transactional
-    public Object modify(String className,String teacherPassword,String teacherNum,Integer id){
-        Map<String,Object> map=new HashMap<>();
-        int i=0,j=0;
-        if (!ObjectUtils.isEmpty(teacherPassword)){
+    public Object modify(String className, String teacherPassword, String teacherNum, Integer id) {
+        Map<String, Object> map = new HashMap<>();
+        int i = 0, j = 0;
+        if (!ObjectUtils.isEmpty(teacherPassword)) {
             Teacher teacher = teacherMapper.selectByPrimaryKey(id);
             teacherPassword = DigestUtils.md5DigestAsHex(teacherPassword.getBytes());
             teacher.setTeacherPassword(teacherPassword);
-            loger.info("修改登录密码：{}",teacherPassword);
+            loger.info("修改登录密码：{}", teacherPassword);
             i = teacherMapper.updateByPrimaryKeySelective(teacher);
-            if (i<=0){
+            if (i <= 0) {
                 throw new RuntimeException();
             }
         }
-        if (!ObjectUtils.isEmpty(className)){
+        if (!ObjectUtils.isEmpty(className)) {
             classInfoMapper.updateHead_Tescher(teacherNum);
             classInfoMapper.updatehead_master_numByclass_name(className, teacherNum);
         }
         map.put("code", 0);
-        map.put("msg","成功！");
+        map.put("msg", "成功！");
         return map;
     }
-
 
 
 }
